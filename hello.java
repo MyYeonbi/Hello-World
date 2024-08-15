44aec2f0800842c8c6b23ab91e35553ccc359c83
MyYeonbi <yeonbi_kim@icloud.com>
2024-08-15 18:36:33 +0900

    Reapply "hi+yeon"
    This reverts commit 04d97adf18a9eeb831f39cff89c1909c63cbc91c.

--- hello.java
+++ hello.java
@@ -3,5 +3,9 @@ public class hello {
         int a = 3;
         int b = 5;
         System.out.println("a + b ="+ a+b);
+
+        String c= "hi";
+        String d = "yeon";
+        System.out.println(c+d);
     }
 }
